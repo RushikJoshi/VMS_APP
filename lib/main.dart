e34ac0a70d45e78b884d39fd80063a681240e451
MAYUR CHAVDA <mayurchavda1210@gmail.com>
2026-05-26 11:11:09 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'features/auth/presentation/pages/splash_screen.dart';
-
 import 'core/dependency_injection/injection_container.dart' as di;
-import 'features/company_admin_dashboard/presentation/pages/company_admin_dashboard_tab.dart';
-import 'features/company_admin_dashboard/presentation/pages/company_admin_layout.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
